59285c4e4956992b0b2b301629ee9776d56cb7f9
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-12-07 14:28:32 -0300

    refactor: controllers - um melhor desempenho

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ app.use(cors());
 app.use(
     express.urlencoded({
         extended: true,
+        limit: "2000mb",
     })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "2000mb" }));
+
 app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 //IMPLEMENTAR ROTAS DA API AQUI
